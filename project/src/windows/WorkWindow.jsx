94de7d45f908b0e57ab5975154aecd4b7e1087a2
muuskrat <mariogon858@gmail.com>
2025-05-07 13:34:55 -0700

    Added basic work window view

--- project/src/windows/WorkWindow.jsx
+++ project/src/windows/WorkWindow.jsx
@@ -4,48 +4,47 @@ import { useDraggableWindow } from '../components/useDraggableWindow';
 import '../styles/WorkWindow.css';
 
 export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
-    const {
-        windowRef,
-        headerRef,
-        dragStyle,
-        handleDragEnd,
-        attributes,
-        listeners,
-        setNodeRef
-    } = useDraggableWindow('Work');
+  const {
+      windowRef,
+      headerRef,
+      dragStyle,
+      handleDragEnd,
+      attributes,
+      listeners,
+      setNodeRef
+  } = useDraggableWindow('Work');
 
-    const box = {
-        width: 100,
-        height: 100,
-        backgroundColor: "#9911ff",
-        borderRadius: 5,
-        
-    }
-
-    function SkillBoxes() {
-        const skills = ['C', 'C++', 'Java', 'Python', 'JavaScript', 'HTML', 'CSS', 'React', ];
+  const imagebox = {
+      width: 200,
+      height: 250,
+      backgroundColor: "#9911ff",
+      borderRadius: 5,
       
-        return (
-          <div  style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, minmax(50px, 1fr))',
-            gap: '12px',
-            padding: '12px'
-          }}>
-            {skills.map(skill => (
-              <motion.div
-                key={skill}
-                whileHover={{ scale: 0.95 }}
-                transition={{ duration: .5 }}
-                className="skill-box"
-              >
-                {skill}
-              </motion.div>
-            ))}
-          </div>
-        );
-      }
+  }
 
+  function SkillBoxes({ skills }) {
+      
+    
+      return (
+        <div  style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(50px, 1fr))',
+          gap: '12px',
+          padding: '12px'
+        }}>
+          {skills.map(skill => (
+            <motion.div
+              key={skill}
+              whileHover={{ scale: 0.95 }}
+              transition={{ duration: .5 }}
+              className="skill-box"
+            >
+              {skill}
+            </motion.div>
+          ))}
+        </div>
+      );
+    }
   
 
     return (
@@ -71,9 +70,54 @@ export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
 
                     <div className="window-content-work">
                         <h2>Development Skills</h2>
-                        <SkillBoxes />
+                        <SkillBoxes skills={['C', 'C++', 'Java', 'Python', 'JS', 'HTML', 'CSS', 'React']}/>
                         <div class="divider"></div>
                         <h2>Projects</h2>
+                        <div className="projects">
+
+                          
+                          <div className="test-work-container">
+                            <div className="test-work-container-intro">
+                              <motion.div className="test-work-image" 
+                                whileHover={{scale: 1.1}}
+                                style={imagebox}></motion.div>
+                              <div className="test-work-content">
+                                <h2 className="test-work-title">hello</h2>
+                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
+                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="divider"></div>
+                          <div className="test-work-container">
+                            <div className="test-work-container-intro">
+                              <motion.div className="test-work-image" 
+                                whileHover={{scale: 1.1}}
+                                style={imagebox}></motion.div>
+                              <div className="test-work-content">
+                                <h2 className="test-work-title">hello</h2>
+                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
+                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="divider"></div>
+                          <div className="test-work-container">
+                            <div className="test-work-container-intro">
+                              <motion.div className="test-work-image" 
+                                whileHover={{scale: 1.1}}
+                                style={imagebox}></motion.div>
+                              <div className="test-work-content">
+                                <h2 className="test-work-title">hello</h2>
+                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
+                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
                     </div>
             </motion.div>
         </div>
