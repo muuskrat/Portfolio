1f4957c9a070cf91e71d0323e07253f8cadd0901
muuskrat <mariogon858@gmail.com>
2025-05-08 14:37:59 -0700

    Made WorkWindows main card a reusable function and added some stylings. Implemented the first card

--- project/src/windows/WorkWindow.jsx
+++ project/src/windows/WorkWindow.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence, createBox } from "framer-motion"; 
 import { useDraggableWindow } from '../components/useDraggableWindow';
 import '../styles/WorkWindow.css';
+import img1 from '../assets/image.png';
+import horse from '../assets/horse.png';
 
 export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
   const {
@@ -23,12 +25,11 @@ export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
   }
 
   function SkillBoxes({ skills }) {
-      
-    
       return (
         <div  style={{
           display: 'grid',
           gridTemplateColumns: 'repeat(auto-fit, minmax(50px, 1fr))',
+          //maxWidth: '200px',
           gap: '12px',
           padding: '12px'
         }}>
@@ -45,6 +46,47 @@ export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
         </div>
       );
     }
+
+    function ProjectCard({title, skills, desc1, desc2, imageStyle}){
+      return(
+        <div className="work-container">
+          <div className="work-container-intro">
+            <motion.div className="work-image" 
+              whileHover={{scale: 1.1}}
+              style={imageStyle}
+            ></motion.div>
+            <div className="work-content">
+              <h2 className="work-title">{title}</h2>
+                <SkillBoxes className="work-skills" skills={skills}/>
+              <div className="work-desc1">{desc1}</div>
+              <div className="work-desc2">{desc2}</div>
+            </div>
+          </div>
+        </div>
+  );
+}
+
+const projects = [
+  { 
+    title: 'Horse Race Game', 
+    skills: ['C++', 'SDL', ], 
+    desc1: 'A 2D horse racing game with an integrated horse breeding system, built using SDL (Simple DirectMedia Layer). Players can race their horses, manage stats like speed and stamina, and breed horses to develop stronger generations over time. Features include randomized genetics, dynamic race outcomes, and a retro-inspired pixel art style.',
+    desc2: 'Designed and programmed this game from scratch to explore game development fundamentals, simulation mechanics, and low-level graphics programming.',
+    image: horse 
+  },
+  { 
+    title: 'world', 
+    skills: ['C++', 'Python'], 
+    desc: 'Lorem ipsum...',
+    image: img1
+  },
+  { 
+    title: 'foo', 
+    skills: ['Java', 'React'], 
+    desc: 'Lorem ipsum...',
+    image: '/myimage3.jpg'
+  },
+];
   
 
     return (
@@ -74,48 +116,25 @@ export const Work = ({ onClose, onBringToFront, zIndex, openWindow }) => {
                         <div class="divider"></div>
                         <h2>Projects</h2>
                         <div className="projects">
-
-                          
-                          <div className="test-work-container">
-                            <div className="test-work-container-intro">
-                              <motion.div className="test-work-image" 
-                                whileHover={{scale: 1.1}}
-                                style={imagebox}></motion.div>
-                              <div className="test-work-content">
-                                <h2 className="test-work-title">hello</h2>
-                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
-                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
-                              </div>
-                            </div>
-                          </div>
-
-                          <div class="divider"></div>
-                          <div className="test-work-container">
-                            <div className="test-work-container-intro">
-                              <motion.div className="test-work-image" 
-                                whileHover={{scale: 1.1}}
-                                style={imagebox}></motion.div>
-                              <div className="test-work-content">
-                                <h2 className="test-work-title">hello</h2>
-                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
-                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
-                              </div>
-                            </div>
-                          </div>
-
-                          <div class="divider"></div>
-                          <div className="test-work-container">
-                            <div className="test-work-container-intro">
-                              <motion.div className="test-work-image" 
-                                whileHover={{scale: 1.1}}
-                                style={imagebox}></motion.div>
-                              <div className="test-work-content">
-                                <h2 className="test-work-title">hello</h2>
-                                  <SkillBoxes className="test-work-skills" skills={['JS', 'HTML', 'CSS', 'React']}/>
-                                <div className="test-work-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex veniam magnam aliquid labore nesciunt est provident fuga corrupti consectetur, possimus assumenda iure esse asperiores ipsa? Quos distinctio perferendis odit magnam!</div>
-                              </div>
-                            </div>
-                          </div>
+                          {projects.map((project, index) => (
+                            <React.Fragment key={index}>
+                            <ProjectCard 
+                              title={project.title}
+                              skills={project.skills}
+                              desc1={project.desc1}
+                              desc2={project.desc2}
+                              imageStyle={{
+                                ...imagebox,
+                                backgroundImage: `url(${project.image})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center'
+                              }
+                              }
+                            />
+                            {index !== projects.length - 1 && <div className="divider"></div>}
+                            </React.Fragment>
+                          ))}
+                            
 
                         </div>
                     </div>
